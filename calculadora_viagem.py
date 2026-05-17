45a5db5216ad61f54c625896bb70df337a887e54
Alexia Fuly <alexiafuly@eq.ufrj.br>
2026-05-16 21:42:26 -0300

    Refine comments for clarity and consistency

--- calculadora_viagem.py
+++ calculadora_viagem.py
@@ -1,7 +1,7 @@
 from IPython.core.formatters import format_display_data
 # 1 - Entrada de dados e tipos
 
-## Definindo as variáveis de entrada com uma restrição - Se o usuário inserir valores inválidos (negativos), o programa exibe uma mensagem de erro e para de executar.
+## Definindo as variáveis de entrada com uma restrição: se o usuário inserir valores inválidos (negativos), o programa exibe uma mensagem de erro e para de executar.
 
 orçamento = float(input("Digite o orçamento disponível para a viagem em reais: "))
 if orçamento < 0:
@@ -24,38 +24,39 @@ if qtd_dias < 0:
 
 # 2 - Cálculos e lógica
 
-## Conversão do custo diário de euros pra reais (1 EUR = 6.10 BRL)
+## Conversão do custo diário de euros pra reais (1 EUR = 6.10 BRL).
 custo_diario_BRL = custo_diario * 6.10
 
-## Cálculo da hospedagem - multiplicação do custo diário (em reais) pela quantidade de dias
+## Cálculo da hospedagem - Multiplicação do custo diário (em reais) pela quantidade de dias.
 custo_hospedagem = custo_diario_BRL * qtd_dias
 
-## Custo total da viagem - soma dos gastos com passagem e hospedagem em reais
+## Custo total da viagem - Soma dos gastos com passagem e hospedagem em reais.
 custo_total = custo_passagem + custo_hospedagem
 
-## Validação de orçamento -
+## Validação de orçamento - Definindo o orçamento como possível se ele for igual ou maior que o custo total.
 Orçamento_possível = ()
 if custo_total <= orçamento:
   Orçamento_possível = True
 else:
-  Orçamento_impossível = True
+  Orçamento_possível = False
 
-## Status final da viagem -
+## Status final da viagem - Definindo a viagem como viável se o orçamento for igual ou maior que o custo total e a duração for maior que 0 dias.
 viável = ()
-if custo_total < orçamento and qtd_dias > 0:
+if custo_total <= orçamento and qtd_dias > 0:
   viável = True
 else:
-  inviável = True
+  viável = False
 
-## Cálculo da diferença - quanto sobra ou quanto falta no orçamento para cobrir o custo total da viagem
+## Cálculo da diferença - Definindo se vai faltar ou sobrar dinheiro no orçamento para cobrir o custo total da viagem.
 diferença = orçamento - custo_total
 falta = ()
 if diferença < 0:
   falta = True
 else:
+  falta = False
   sobra = True
 
-# 3 - Exibição de resultados
+# 3 - Exibição de resultados - Printando o resultado dos cálculos utilizando f strings e condicionais.
 
 print(f"O custo total da hospedagem em reais é: {round(custo_hospedagem,2)} reais")
 
